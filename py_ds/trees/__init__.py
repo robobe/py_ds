e32e7078973f018693afd795e356a1b0600e08c3
robo <robo2020@gmail.com>
2022-07-16 12:09:25 +0300

    add print_tree method

--- py_ds/trees/__init__.py
+++ py_ds/trees/__init__.py
@@ -1,10 +1,15 @@
 class TreeNode():
     def __init__(self, data) -> None:
-        self.data = None
+        self.data = data
         self.parent = None
-        self.children = None
+        self.children = []
 
-
+    def add_child(self, data):
+        node = TreeNode(data)
+        node.parent = self
+        
+        self.children.append(node)
+        return node
 
 class Tree():
     def __init__(self) -> None:
@@ -12,15 +17,18 @@ class Tree():
         self.root.children = []
 
 
-    def add_child(self, parent, data):
-        node = TreeNode(data)
-        node.children = []
-        node.parent = parent
+    
         
-        parent.children.append(self)
-        
-        
+    def print_tree(self, node: TreeNode) -> None:
+        print(node.data)
+        for child in node.children:
+            self.print_tree(child)
 
 if __name__ == "__main__":
     tree = Tree()
-    tree.add_child(tree.root, "node1")
+    n1 = tree.root.add_child("node1")
+    n2 = tree.root.add_child("node2")
+    n3 = tree.root.add_child("node3")
+    n21 = n2.add_child("node21")
+    n22 = n2.add_child("node22")
+    tree.print_tree(tree.root)
